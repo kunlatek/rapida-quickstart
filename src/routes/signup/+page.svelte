f786da5b6c780d3e07b39ebe60abcc9f0ddea1bd
Alexis de Barros <apenasalexis@gmail.com>
2024-07-04 19:55:45 +0000

    :sparkles: implements home

--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -1,83 +1,95 @@
 <script lang="ts">
-  import { Input, Label, Button } from 'flowbite-svelte';
-  import { signup } from '$lib/services/authService';
-  import { onMount } from 'svelte';
-  import { writable } from 'svelte/store';
-  import { goto } from '$app/navigation';
+	import { Input, Label, Button } from 'flowbite-svelte';
+	import { signup } from '$lib/services/authService';
+	import { writable } from 'svelte/store';
+	import { goto } from '$app/navigation';
 	import type { IRequestErrorResponse } from '../../types/rest';
 	import { handleApiError } from '../../utils/errorHandler';
 
-  const email = writable('');
-  const password = writable('');
-  const confirmPassword = writable('');
-  const errorMessage = writable('');
+	const email = writable('');
+	const password = writable('');
+	const confirmPassword = writable('');
+	const errorMessage = writable('');
 
-  onMount(() => {
-    const url = new URL(window.location.href);
-    $email = url.searchParams.get('email') || '';
-    if ($email === '') {
-      goto('/');
-    }
-  });
+	async function handleSubmit(event: Event) {
+		event.preventDefault();
 
-  async function handleSubmit(event: Event) {
-    event.preventDefault();
+		if ($password !== $confirmPassword) {
+			errorMessage.set('Senhas não coincidem. Por favor, tente novamente.');
+			return;
+		}
 
-    if ($password !== $confirmPassword) {
-      errorMessage.set('Senhas não coincidem. Por favor, tente novamente.');
-      return;
-    }
-
-    try {
-      const response = await signup($email, $password);
-      console.log('Signup successful:', response);
-      // Redirecionar para a página de login ou outra página após o registro
-      goto('/login');
-    } catch (error: IRequestErrorResponse | any) {
-      errorMessage.set(handleApiError(error));
-    }
-  }
+		try {
+			const response = await signup($email, $password);
+			console.log('Signup successful:', response);
+			// Redirecionar para a página de login ou outra página após o registro
+			goto('/login');
+		} catch (error: IRequestErrorResponse | any) {
+			errorMessage.set(handleApiError(error));
+		}
+	}
 </script>
 
-<div class="absolute inset-0 flex justify-center items-center bg-default">
-  <form on:submit={handleSubmit} class="max-w-md w-full bg-white rounded-lg shadow-md p-8 dark:bg-gray-800 dark:text-white">
-    <!-- Logo Section -->
-    <div class="flex justify-center mb-8">
-      <img src="/images/logo.png" alt="Logo Kunlatek" class="h-12 w-auto">
-    </div>
-    <!-- Form Section -->
-    <div class="mb-6">
-      <Label for="email" class="mb-2">E-mail pelo qual foi convidado</Label>
-      <Input type="email" id="email" bind:value={$email} placeholder="john.doe@company.com" required disabled />
-    </div>
-    <div class="mb-6">
-      <Label for="password" class="mb-2">Senha</Label>
-      <Input type="password" id="password" bind:value={$password} placeholder="•••••••••" required />
-    </div>
-    <div class="mb-6">
-      <Label for="confirmPassword" class="mb-2">Confirmar Senha</Label>
-      <Input type="password" id="confirmPassword" bind:value={$confirmPassword} placeholder="•••••••••" required />
-    </div>
-    {#if $errorMessage}
-      <p class="text-red-500">{$errorMessage}</p>
-    {/if}
-    <Button type="submit" outline>Salvar</Button>
-  </form>
+<div class="bg-default absolute inset-0 flex items-center justify-center">
+	<form
+		on:submit={handleSubmit}
+		class="w-full max-w-md rounded-lg bg-white p-8 shadow-md dark:bg-gray-800 dark:text-white"
+	>
+		<!-- Logo Section -->
+		<div class="mb-8 flex justify-center">
+			<img src="/images/logo.png" alt="Logo Kunlatek" class="h-12 w-auto" />
+		</div>
+		<!-- Form Section -->
+		<div class="mb-6">
+			<Label for="email" class="mb-2">E-mail pelo qual foi convidado</Label>
+			<Input
+				type="email"
+				id="email"
+				bind:value={$email}
+				placeholder="john.doe@company.com"
+				required
+			/>
+		</div>
+		<div class="mb-6">
+			<Label for="password" class="mb-2">Senha</Label>
+			<Input
+				type="password"
+				id="password"
+				bind:value={$password}
+				placeholder="•••••••••"
+				required
+			/>
+		</div>
+		<div class="mb-6">
+			<Label for="confirmPassword" class="mb-2">Confirmar Senha</Label>
+			<Input
+				type="password"
+				id="confirmPassword"
+				bind:value={$confirmPassword}
+				placeholder="•••••••••"
+				required
+			/>
+		</div>
+		{#if $errorMessage}
+			<p class="text-red-500">{$errorMessage}</p>
+		{/if}
+		<Button type="submit" outline>Salvar</Button>
+	</form>
 </div>
 
 <style>
-  .bg-default {
-    background-color: var(--bg-color);
-  }
+	.bg-default {
+		background-color: var(--bg-color);
+	}
 
-  @media (max-width: 640px) {
-    .max-w-md {
-      width: 100%;
-      padding: 16px;
-    }
+	@media (max-width: 640px) {
+		.max-w-md {
+			width: 100%;
+			padding: 16px;
+		}
 
-    .h-12 {
-      height: 8rem;
-    }
-  }
+		.h-12 {
+			height: 8rem;
+		}
+	}
 </style>
