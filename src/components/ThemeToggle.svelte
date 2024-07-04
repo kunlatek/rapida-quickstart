90ccc9a74b969abe9d0ac531d37e8a665465afbd
Alexis de Barros <apenasalexis@gmail.com>
2024-07-04 12:19:06 +0000

    :bug: fix dark mode toggle

--- src/components/ThemeToggle.svelte
+++ src/components/ThemeToggle.svelte
@@ -1,28 +1,31 @@
 <script>
-  import { onMount } from 'svelte';
-  import { Toggle } from 'flowbite-svelte';
+	import { onMount } from 'svelte';
+	import { Toggle } from 'flowbite-svelte';
 
-  let theme = 'dark';
+	let theme = 'light';
 
-  const toggleTheme = () => {
-    theme = theme === 'dark' ? 'light' : 'dark';
-    if (typeof window !== 'undefined') {
-      localStorage.setItem('theme', theme);
-      document.documentElement.classList.toggle('light', theme === 'light');
-    }
-  };
+	const toggleTheme = () => {
+		if (typeof window === 'undefined') return;
 
-  onMount(() => {
-    if (typeof window !== 'undefined') {
-      const storedTheme = localStorage.getItem('theme');
-      if (storedTheme) {
-        theme = storedTheme;
-        document.documentElement.classList.add(theme);
-      } else {
-        localStorage.setItem('theme', theme);
-      }
-    }
-  });
+		const classList = document.documentElement.classList;
+		theme = theme === 'dark' ? 'light' : 'dark';
+		if (classList.contains('dark')) classList.remove('dark');
+		else classList.add('dark');
+		localStorage.setItem('theme', theme);
+	};
+
+	const capitalizeFirstLetter = (/** @type {string} */ term) => {
+		return term.charAt(0).toUpperCase() + term.slice(1);
+	};
+
+	onMount(() => {
+		if (typeof window === 'undefined') return;
+
+		const storedTheme = localStorage.getItem('theme');
+		if (storedTheme !== theme) toggleTheme();
+	});
 </script>
 
-<Toggle on:click={toggleTheme}>Light mode</Toggle>
+<Toggle on:click={toggleTheme} checked={theme === 'dark'}>
+	{capitalizeFirstLetter(theme)} mode
+</Toggle>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,57 +1,58 @@
 import flowbitePlugin from 'flowbite/plugin';
 
 export default {
-  content: [
-    './src/**/*.{html,js,svelte,ts}',
-    './node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'
-  ],
-  theme: {
-    extend: {
-      colors: {
-        // Definindo cores do tema light
-        light: {
-          background: '#ffffff',
-          text: '#000000',
-          primary: {
-            50: '#FFF5F2',
-            100: '#FFF1EE',
-            200: '#FFE4DE',
-            300: '#FFD5CC',
-            400: '#FFBCAD',
-            500: '#FE795D',
-            600: '#EF562F',
-            700: '#EB4F27',
-            800: '#CC4522',
-            900: '#A5371B'
-          },
-        },
-        // Definindo cores do tema dark
-        dark: {
-          background: '#1e293b',
-          text: '#ffffff',
-          primary: {
-            50: '#E0F7FF',
-            100: '#B3EFFF',
-            200: '#80E4FF',
-            300: '#4DD9FF',
-            400: '#26CFFF',
-            500: '#00C4FF',
-            600: '#00A8D9',
-            700: '#008CB3',
-            800: '#00708D',
-            900: '#005467'
-          }
-        }
-      },
-      backgroundColor: theme => ({
-        'light-background': theme('colors.light.background'),
-        'dark-background': theme('colors.dark.background')
-      }),
-      textColor: theme => ({
-        'light-text': theme('colors.light.text'),
-        'dark-text': theme('colors.dark.text')
-      })
-    }
-  },
-  plugins: [flowbitePlugin]
+	darkMode: 'class',
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		'./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'
+	],
+	theme: {
+		extend: {
+			colors: {
+				// Definindo cores do tema light
+				light: {
+					background: '#ffffff',
+					text: '#000000',
+					primary: {
+						50: '#FFF5F2',
+						100: '#FFF1EE',
+						200: '#FFE4DE',
+						300: '#FFD5CC',
+						400: '#FFBCAD',
+						500: '#FE795D',
+						600: '#EF562F',
+						700: '#EB4F27',
+						800: '#CC4522',
+						900: '#A5371B'
+					}
+				},
+				// Definindo cores do tema dark
+				dark: {
+					background: '#1e293b',
+					text: '#ffffff',
+					primary: {
+						50: '#E0F7FF',
+						100: '#B3EFFF',
+						200: '#80E4FF',
+						300: '#4DD9FF',
+						400: '#26CFFF',
+						500: '#00C4FF',
+						600: '#00A8D9',
+						700: '#008CB3',
+						800: '#00708D',
+						900: '#005467'
+					}
+				}
+			},
+			backgroundColor: (theme) => ({
+				'light-background': theme('colors.light.background'),
+				'dark-background': theme('colors.dark.background')
+			}),
+			textColor: (theme) => ({
+				'light-text': theme('colors.light.text'),
+				'dark-text': theme('colors.dark.text')
+			})
+		}
+	},
+	plugins: [flowbitePlugin]
 };
