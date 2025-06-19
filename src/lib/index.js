34a8ed53ce52e749ba9fa8f357f80d8d2364df33
Ryzzan Abbade Salman <ryzzan.salman.ext@rendimento.com.br>
2025-06-19 20:04:43 -0300

    :sparkles: Atualizar a estrutura de exportação em index.js para incluir novos serviços e componentes reutilizáveis. Modificar o package.json para adicionar o script de preparação do SvelteKit.

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,12 +1,52 @@
 // Stores
-export * as authStore from './stores/auth';
-export * as toastStore from './stores/toast';
-export * as profileStore from './stores/profile';
+export { authStore, getState as getAuthState } from "./stores/auth";
+export {
+  profileStore,
+  getState as getProfileState,
+  loadProfiles,
+  clearProfiles,
+  updateProfile,
+} from "./stores/profile";
+export * from "./stores/toast";
+export * from "./stores/account-deletion";
 
 // Services
-export * as authService from './services/auth';
-export * as profileService from './services/profile';
-export * as apiService from './services/api';
+export * from "./services/auth";
+export * from "./services/profile";
+export * from "./services/api";
+export * from "./services/invitations";
+export * from "./services/user";
+export * from "./services/errorMapper";
 
-// Components
-export * as components from './components';
+// --- Reusable Components ---
+
+// Common Components
+export { default as KuLoading } from "./components/common/KuLoading.svelte";
+export { default as KuToast } from "./components/common/KuToast.svelte";
+export { default as KuNavbar } from "./components/common/KuNavbar.svelte";
+export { default as KuFooter } from "./components/common/KuFooter.svelte";
+export { default as KuModal } from "./components/common/KuModal.svelte";
+
+// Data Components
+export { default as DataCard } from "./components/data/DataCard.svelte";
+export { default as KuDataTable } from "./components/data/KuDataTable.svelte";
+
+// Form Components
+export { default as KuInput } from "./components/form/KuInput.svelte";
+export { default as KuSelect } from "./components/form/KuSelect.svelte";
+export { default as KuFile } from "./components/form/KuFile.svelte";
+export { default as KuFieldset } from "./components/form/KuFieldset.svelte";
+export { default as KuTab } from "./components/form/KuTab.svelte";
+export { default as KuButton } from "./components/form/KuButton.svelte";
+export { default as KuAutocomplete } from "./components/form/KuAutocomplete.svelte";
+export { default as KuArray } from "./components/form/KuArray.svelte";
+
+// Layout Components
+export { default as AuthLayout } from "./components/layout/AuthLayout.svelte";
+export { default as Panel } from "./components/layout/Panel.svelte";
+
+// Navigation Components
+export { default as KuPagination } from "./components/navigation/KuPagination.svelte";
+
+// Theme Components
+export { default as ThemeProvider } from "./components/theme/ThemeProvider.svelte";
