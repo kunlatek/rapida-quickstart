dd3bf98d93a3091d7ab65b4f546de6e15045a74e
Ryzzan Abbade Salman <ryzzan.salman.ext@rendimento.com.br>
2025-07-05 13:12:34 -0300

    :art: Atualizar configuração do Vite para incluir proxy da API e definir variáveis de ambiente

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,26 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
 
 export default defineConfig({
- plugins: [sveltekit()],
- server: {
-  fs: {
-   allow: ['..', 'all']
+  plugins: [sveltekit()],
+  server: {
+    fs: {
+      allow: ["..", "all"],
+    },
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+    define: {
+      "import.meta.env.VITE_GOOGLE_CLIENT_ID": JSON.stringify(
+        process.env.VITE_GOOGLE_CLIENT_ID
+      ),
+      "import.meta.env.VITE_APPLE_CLIENT_ID": JSON.stringify(
+        process.env.VITE_APPLE_CLIENT_ID
+      ),
+    },
   },
-  proxy: {
-   // Configuração do proxy para a API
-   '/api': {
-     target: 'http://localhost:3000',
-     changeOrigin: true,
-     rewrite: (path) => path.replace(/^\/api/, '')
-   }
-  },
-  define: {
-    'import.meta.env.VITE_GOOGLE_CLIENT_ID': JSON.stringify(process.env.VITE_GOOGLE_CLIENT_ID),
-    'import.meta.env.VITE_APPLE_CLIENT_ID': JSON.stringify(process.env.VITE_APPLE_CLIENT_ID)
-  }
- }
 });
