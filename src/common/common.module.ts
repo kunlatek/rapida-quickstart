c5113d4338a5a0575e117e4097afaca7ae949467
Ryzzan Abbade Salman <ryzzan.salman.ext@rendimento.com.br>
2025-07-08 23:34:43 -0300

    :sparkles: Adicionar ConfigModule ao CommonModule para suporte à configuração

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -3,8 +3,10 @@ import { ErrorService } from "./services/error.service";
 import { CascadeService } from "./services/cascade.service";
 import { DiscordLoggerService } from "./services/discord-logger.service";
 import { UploadService } from "./services/upload.service";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
+  imports: [ConfigModule],
   providers: [
     ErrorService,
     CascadeService,
