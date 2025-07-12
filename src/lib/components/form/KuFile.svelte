706d86ed62776bbd408c7c31f25c5d17fcebac74
Ryzzan Abbade Salman <ryzzan.salman.ext@rendimento.com.br>
2025-07-12 15:55:57 -0300

    :art: Substituir componentes PaperclipSolid e TrashBinSolid por SVGs no componente KuFile e remover log de depuração

--- src/lib/components/form/KuFile.svelte
+++ src/lib/components/form/KuFile.svelte
@@ -3,7 +3,6 @@
 	import { getComponentClasses } from '$lib/styles/theme';
 	import type { IFormCondition } from '../../interfaces/form.interfaces';
 	import { createEventDispatcher } from 'svelte';
-	import { PaperclipSolid, TrashBinSolid } from 'flowbite-svelte-icons';
 
 	interface FileVariant {
 		base: string;
@@ -18,7 +17,7 @@
 	export let name = '';
 	export let label = '';
 	export let value: FileList | string | null = null;
-	export const placeholder = '';
+	export let placeholder = '';
 	export let tooltip = '';
 	export let isDisabled = false;
 	export let isRequired = false;
@@ -30,10 +29,6 @@
 	export let validators: ('onlyImages' | 'png' | 'jpg' | 'pdf')[] = [];
 
 	const dispatch = createEventDispatcher();
-    
-    // --- DEBUG LOG ---
-    $: console.log(`[KuFile: '${name}'] received value:`, value, `(Type: ${typeof value})`);
-    // --- END DEBUG LOG ---
 
 	$: themeClasses = getComponentClasses('file', variant, {
 		error: !!error,
@@ -173,11 +168,35 @@
 					rel="noopener noreferrer"
 					class="flex items-center text-blue-600 dark:text-blue-500 hover:underline"
 				>
-					<PaperclipSolid class="w-4 h-4 mr-2" />
+					<svg
+						class="w-5 h-5 mr-2"
+						fill="currentColor"
+						viewBox="0 0 20 20"
+						xmlns="http://www.w3.org/2000/svg"
+						aria-hidden="true"
+					>
+						<path
+							clip-rule="evenodd"
+							fill-rule="evenodd"
+							d="M12.586 4.586a2 2 0 112.828 2.828l-3 3a2 2 0 01-2.828 0 1 1 0 00-1.414 1.414 4 4 0 005.656 0l3-3a4 4 0 00-5.656-5.656l-1.5 1.5a1 1 0 101.414 1.414l1.5-1.5zm-5 5a2 2 0 012.828 0 1 1 0 101.414-1.414 4 4 0 00-5.656 0l-3 3a4 4 0 105.656 5.656l1.5-1.5a1 1 0 10-1.414-1.414l-1.5 1.5a2 2 0 11-2.828-2.828l3-3z"
+						></path>
+					</svg>
 					<span class="truncate">{getFileName(value)}</span>
 				</a>
 				<Button size="xs" color="alternative" on:click={removeExistingFile}>
-					<TrashBinSolid class="w-4 h-4" />
+					<svg
+						class="w-4 h-4"
+						fill="currentColor"
+						viewBox="0 0 20 20"
+						xmlns="http://www.w3.org/2000/svg"
+						aria-hidden="true"
+					>
+						<path
+							clip-rule="evenodd"
+							fill-rule="evenodd"
+							d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+						></path>
+					</svg>
 				</Button>
 			</div>
 		{:else}
