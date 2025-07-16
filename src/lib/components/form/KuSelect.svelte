2cd98eef256643b960b988e90ee2fbf3d34b52f0
Ryzzan Abbade Salman <ryzzan.salman.ext@rendimento.com.br>
2025-07-16 19:35:17 -0300

    :art: Melhorar reatividade e corrigir formatação no componente KuSelect, incluindo ajustes na exibição de tooltip e mensagens de erro

--- src/lib/components/form/KuSelect.svelte
+++ src/lib/components/form/KuSelect.svelte
@@ -100,7 +100,7 @@
 		} else {
 			value.push(optionValue);
 		}
-		value = value;
+		value = value; // Trigger Svelte reactivity
 		dispatch('change', value);
 	}
 
@@ -113,22 +113,19 @@
 			{label}
 			{#if isRequired}<span class="text-red-500">*</span>{/if}
 			{#if tooltip}
-				<span
-					class="ml-1 text-gray-400 hover:text-gray-600 cursor-help"
-					title={tooltip}
-				>
+				<span class="ml-1 text-gray-400 hover:text-gray-600 cursor-help" title={tooltip}>
 					<svg
 						class="w-4 h-4 inline"
 						fill="currentColor"
 						viewBox="0 0 20 20"
-						xmlns="http:
+						xmlns="http://www.w3.org/2000/svg"
 					>
 						<path
-							fill-rule="evenodd"
+							fillRule="evenodd"
 							d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
-							clip-rule="evenodd"
+							clipRule="evenodd"
 						></path>
-					</svg>
+						</svg>
 				</span>
 			{/if}
 		</Label>
@@ -175,6 +172,7 @@
 				autofocus={isAutofocus}
 			/>
 		{/if}
+
 		{#if error}
 			<Helper class="mt-1 text-red-600 dark:text-red-500">{error}</Helper>
 		{/if}
