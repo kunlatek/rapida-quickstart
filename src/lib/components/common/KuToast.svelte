c0c6eab971aa3167bfbbd059e86cf269841c7c96
Ryzzan Abbade Salman <ryzzan.salman.ext@rendimento.com.br>
2025-07-24 11:28:52 -0300

    Refatorar componentes KuLoading e KuToast para melhorar a legibilidade e a estrutura do código

--- src/lib/components/common/KuToast.svelte
+++ src/lib/components/common/KuToast.svelte
@@ -3,7 +3,6 @@
   import { Toast as FlowbiteToast } from "flowbite-svelte";
   import { fly } from "svelte/transition";
 
-  // Tipos válidos do Flowbite Toast
   type FlowbiteToastColor =
     | "none"
     | "red"
@@ -17,7 +16,6 @@
     | "primary"
     | undefined;
 
-  // Mapeamento de tipos para cores do Flowbite
   const typeClasses: Record<ToastType, FlowbiteToastColor> = {
     success: "green",
     error: "red",
@@ -25,42 +23,40 @@
     info: "blue",
   };
 
-  // Função helper para obter a cor correta
   const getToastColor = (type: ToastType): FlowbiteToastColor => {
     return typeClasses[type];
   };
 
-  // Mapeamento de ícones SVG para cada tipo de toast
   const getIcon = (type: ToastType): string => {
     const icons: Record<ToastType, string> = {
       success: `
-        <path
-          fill-rule="evenodd"
-          d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-          clip-rule="evenodd"
-        />
-      `,
+  <path
+  fill-rule="evenodd"
+  d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+  clip-rule="evenodd"
+  />
+  `,
       error: `
-        <path
-          fill-rule="evenodd"
-          d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z"
-          clip-rule="evenodd"
-        />
-      `,
+  <path
+  fill-rule="evenodd"
+  d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z"
+  clip-rule="evenodd"
+  />
+  `,
       warning: `
-        <path
-          fill-rule="evenodd"
-          d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z"
-          clip-rule="evenodd"
-        />
-      `,
+  <path
+  fill-rule="evenodd"
+  d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z"
+  clip-rule="evenodd"
+  />
+  `,
       info: `
-        <path
-          fill-rule="evenodd"
-          d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v2H7a1 1 0 100 2h2v2a1 1 0 102 0v-2h2a1 1 0 100-2h-2V7z"
-          clip-rule="evenodd"
-        />
-      `,
+  <path
+  fill-rule="evenodd"
+  d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v2H7a1 1 0 100 2h2v2a1 1 0 102 0v-2h2a1 1 0 100-2h-2V7z"
+  clip-rule="evenodd"
+  />
+  `,
     };
     return icons[type];
   };
@@ -85,7 +81,6 @@
         on:close={() => toastStore.remove(toast.id)}
       >
         <div class="flex items-center">
-          <!-- Ícone para cada tipo de toast -->
           <svg
             class="w-5 h-5 mr-2 flex-shrink-0"
             fill="currentColor"
